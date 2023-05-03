3ac3f0a3f69019c19649a8e8567e2642cad1389c
unknown <annieyashley@gmail.com>
2023-05-03 17:01:48 -0500

    framework for inquirer and response functions

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,16 +7,16 @@ INSERT INTO role (id, title, salary, department_id)
 VALUES  (1, "Manager", "100000", 2),
         (2, "Placeholder", "80000", 3),
         (3, "Placeholder", "60000", 1),
-        (4, "Placeholder", "40000", 4),
+        (4, "Placeholder", "40000", 2),
         (5, "Intern", "10", 1);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES  (1, "Jane", "Doe", 1),
-        (2, "B", "C", 2),
-        (3, "D", "E", 3),
-        (4, "F", "G", 4),
-        (6, "H", "I", 5),
-        (7, "J", "K", 2),
-        (8, "L", "M", 1),
-        (9, "N", "O", 3),
-        (10, "P", "Q", 4),
+VALUES  (1, "Jane", "Doe", 1, null),
+        (2, "B", "C", 2, 1),
+        (3, "D", "E", 3, 1),
+        (4, "F", "G", 4, 8),
+        (6, "H", "I", 5, 1),
+        (7, "J", "K", 2, 8),
+        (8, "L", "M", 1, null),
+        (9, "N", "O", 3, 8),
+        (10, "P", "Q", 4, 8);
--- server.js
+++ server.js
@@ -1,3 +1,158 @@
 const inquirer = require("inquirer");
 const express = require('express');
 const mysql = require('mysql2');
+require("dotenv").config()
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    console.log(`Connected to the employee database.`)
+  );
+
+const questionsList = [
+    {
+        name: "mainquestions",
+        message: "Welcome to the employee database. Please select one of the following options:",
+        type: "list",
+        choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role", "Exit"]
+    }
+]
+
+function displayMainQuestions(){
+    inquirer.prompt(questionsList)
+    .then (answers => {
+        switch(answers){
+            case "View All Departments":
+                viewAllDepartments();
+            case "View All Roles":
+                viewAllRoles();
+            case "View All Employees":
+                viewAllEmployees();
+            case "Add a Department":
+                addDepartment();
+            case "Add a Role":
+                addRole();
+            case "Add a Employee":
+                addEmployee();
+            case "Update an Employee Role":
+                updateEmployee();
+            case "Exit":
+                return;
+        }   
+    })
+}
+
+// view departments
+function viewAllDepartments(){
+    db.query("SELECT department.id AS id, department.name FROM department", (err, data) =>
+    {
+        if (err){
+            res.status(400).json({error: err.message})
+            return;
+        };
+        data: data;
+        displayMainQuestions();
+    })
+}
+
+// view roles
+function viewAllRoles(){
+    db.query("SELECT role.id AS id, role.title, department.name FROM role INNER JOIN department ON role.department_id = department.id", (err, data) =>
+    {
+        if (err){
+            res.status(400).json({error: err.message})
+            return;
+        };
+        data: data;
+        displayMainQuestions();
+    })
+}
+
+// view employees
+function viewAllEmployees(){
+    db.query("SELECT employee.id AS id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary FROM employee, role, department WHERE department.id = role.department_id AND role.id = employee.role_id", (err, data) =>
+    {
+        if (err){
+            res.status(400).json({error: err.message})
+            return;
+        };
+        data: data;
+        displayMainQuestions();
+    })
+}
+
+// add a department
+function addDepartment(){
+    db.query("INSERT INTO department (name) VALUES (?)", 
+    
+    inquirer.prompt([
+        {
+            name: "departmentname",
+            message: "What is the name of the new department?",
+            type: "input"
+        }
+    ])
+    .then(answers => {
+        return;
+    })
+    
+    ,(err, data) =>
+    {
+        if (err){
+            res.status(400).json({error: err.message})
+            return;
+        };
+        data: data;
+        displayMainQuestions();
+    })
+}
+
+// add a role
+function addRole(){
+    db.query("", (err, data) =>
+    {
+        if (err){
+            res.status(400).json({error: err.message})
+            return;
+        };
+        data: data;
+        displayMainQuestions();
+    })
+}
+// add an employee
+function addEmployee(){
+    db.query("", (err, data) =>
+    {
+        if (err){
+            res.status(400).json({error: err.message})
+            return;
+        };
+        data: data;
+        displayMainQuestions();
+    })
+}
+
+// update an employee
+function updateEmployee(){
+    db.query("", (err, data) =>
+    {
+        if (err){
+            res.status(400).json({error: err.message})
+            return;
+        };
+        data: data;
+        displayMainQuestions();
+    })
+}
+
+displayMainQuestions();
